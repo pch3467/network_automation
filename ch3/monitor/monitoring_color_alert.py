5507c462ed8cb2974eb1929e320850c7636a9d6a
pch3467 <pch3467a@gmail.com>
2022-12-05 10:30:21 +0900

    12-05

--- ch3/monitor/monitoring_color_alert.py
+++ ch3/monitor/monitoring_color_alert.py
@@ -7,7 +7,11 @@ import os
 import sys
 import datetime
 
-# 로그 전체 내용을 보고 단어의 위치를 알아봅니다.
+# 색상 코드
+errcolor_fatal= '\033[31m'
+errcolor_none = '\033[0m'
+errcolor_except = '\x1b[36m'
+
 def check(file_name, search_word) :
     if os.path.exists(file_name):
         print ("모니터링 시작 :", file_name)
@@ -15,19 +19,16 @@ def check(file_name, search_word) :
         print ("-" * 70)
     else :
         print ("찾으려는 파일이 없습니다 :", file_name)
-
+        
     index = 0
     while os.path.exists(file_name) :
-        # 파일에 찾을 단어('FATAL')가 있는지 확인
         fp = open(file_name)
         file_data =fp.read()
         index = file_data.find(search_word, index)
-        fp.close()
-
-        # 찾았다면 alert 함수로 메시지를 출력하고, 관련 로그의 앞으로 1줄, 뒤로 2줄을 출력
-        # 찾지 못했다면 ...을 출력
+        fp.close() 
+        
         if index >= 0 :
-            alert()
+            alert(search_word)    # 코드 수정
             (data, count) = get_log_data(file_data, search_word, index, 2, 2)
             print (data)
         else :
@@ -35,11 +36,15 @@ def check(file_name, search_word) :
             sys.stdout.flush()
 
         index = len(file_data)
-        sleep(5)                # 5초 동안 쉬기
-
-def alert() :
-    print ("\n", datetime.datetime.now(), "문제가 발생했습니다!!")
-
-# 메인 함수
+        sleep(5)
+        
+# search_word 인자 추가 및 색상 적용(검색한 단어의 종류에 따라 다른 글자색을 적용하도록 수정)
+def alert(search_word) :
+    now = datetime.datetime.now()
+    if search_word == "FATAL" :
+        print (errcolor_fatal + "\n", now, "심각한 문제가 발생했습니다!!" +errcolor_none)
+    elif search_word == "except" :
+        print (errcolor_except + "\n", now, "문제가 발견됐습니다!!" +errcolor_none)
+        
 if __name__ == "__main__":
-    check("/var/log/syslog", "FATAL")
+    check("/var/log/messages", "FATAL")
